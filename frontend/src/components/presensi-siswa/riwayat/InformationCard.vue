da4665270605d942de55aaffcba1658cc3f9d8de
IgnatiusFarel <ignatiusloyolafarel17@gmail.com>
2025-11-23 16:27:59 +0700

    [UPDATE] - Components - Presensi Siswa - Riwayat - Information Card

--- frontend/src/components/presensi-siswa/riwayat/InformationCard.vue
+++ frontend/src/components/presensi-siswa/riwayat/InformationCard.vue
@@ -1,21 +1,23 @@
 <template>
-    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 px-5 mb-4">
- <n-card
+  <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 px-5 mb-4">
+    <n-card
       v-for="item in stats"
       :key="item.title"
       :title="item.title"
       hoverable
-         class="!border-[#C1C2C5] !rounded-xl"
+      class="!border-[#C1C2C5] !rounded-xl"
     >
-      <p class="text-base font-bold">{{ item.count }} / {{ total }} Kegiatan Presensi</p>
+      <p class="text-base font-bold">
+        {{ item.count }} / {{ total }} Kegiatan Presensi
+      </p>
     </n-card>
   </div>
 </template>
 
 <script setup>
-import { NCard } from 'naive-ui';
+import { NCard } from "naive-ui";
 import { ref, onMounted } from "vue";
-import Api from "@/services/Api";
+import Api from "@/services/Api.js";
 
 const loading = ref(false);
 const total = ref(0);
